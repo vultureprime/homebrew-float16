0eb9db34d7db71f34589d39467fcb9f0ccd85684
bossthanawat <boss41680@gmail.com>
2024-10-09 15:09:14 +0700

    Update binary name in Float16Cli formula

--- float16-cli.rb
+++ float16-cli.rb
@@ -1,19 +1,19 @@
 class Float16Cli < Formula
-  desc "CLI for interacting with serverless GPU services"
-  homepage "https://float16.cloud/"
-  url "https://float16.cloud/"
-  version "0.1.0"
-  license "GNU Affero General Public License v3.0"
+  desc "CLI tool for float63"
+  homepage "https://github.com/vultureprime/homebrew-float16"
+  url "https://registry.npmjs.org/@bossthanawat/float63-cli/-/float63-cli-0.0.1-alpha.1.tgz"
+  version "0.0.1-alpha.1"
+  license "ISC"
 
   depends_on "node"
 
   def install
-    system "npm", "install", "-g", "--prefix", libexec, "@bossthanawat/float63-cli"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    output = shell_output("#{bin}/float63 --version")
-    assert_match version.to_s, output
+    # Add a test command here
+    system "#{bin}/float63", "--version"
   end
 end
