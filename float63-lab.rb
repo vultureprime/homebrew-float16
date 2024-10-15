563ea45f4b12cb87a8d39c644917bbdf8cd77fe0
bossthanawat <boss41680@gmail.com>
2024-10-15 17:47:53 +0700

    Update version in Float63Lab formula

--- float63-lab.rb
+++ float63-lab.rb
@@ -3,7 +3,7 @@ class Float63Lab < Formula
   desc "An example formula"
   homepage "https://github.com/vultureprime/homebrew-float16"
   url "https://registry.npmjs.org/@bossthanawat/float63-cli/-/float63-cli-0.0.1-alpha.3.tgz"
-  version "0.0.1-alpha.2"
+  version "0.0.1-alpha.3"
   # sha256 "abc123abc123abc123abc123abc123abc123abc123abc123abc123abc123abc1"
 
   depends_on "node"
